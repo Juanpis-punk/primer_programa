b106f3d781b859a5f7cc835f8f1aa9a3a576f97b
Juanpis <juanpisisonfire@gmail.com>
2026-08-03 19:39:47 -0500

    Java2

---
+++ HolaMundo.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Cuál es tu nombre?: ");
+        String nombre = sc.nextLine();
+       
+
+        
+
+        
+
+
+
+
+
+        
+
+
+
+
+
+    }
+
+
+
+
+}
