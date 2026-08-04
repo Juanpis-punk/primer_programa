5c73d84280e48d71b3b0a048b1fe48591da29f26
Monselmons <ajaraba74@gmail.com>
2026-08-03 20:39:50 -0500

    Cambios de Luis

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,23 +1,27 @@
-
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class HolaMundo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("¿Cuál es tu nombre?: ");
+
         String nombre = sc.nextLine();
-       
 
-        
-
-        
+        JOptionPane.showMessageDialog(null, "Hola mundo.");
 
 
 
 
 
-        
+
+
+
+
+
+
+
 
 
 
