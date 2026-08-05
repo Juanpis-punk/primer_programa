f0fbc87a3027f44f3d0a159f07360d1839b4b330
Juanpis <juanpisisonfire@gmail.com>
2026-08-04 22:09:18 -0500

    Comentarios e Info

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
-import javax.swing.JOptionPane;
+import java.util.Scanner; // Importación de Scanner
+import javax.swing.JOptionPane; // Importación de JOptionPane
 
 public class HolaMundo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("¿Cuál es tu nombre?: ");
+        System.out.print("¿Cuál es tu nombre?: "); // Solicitar nombre al usuario
 
-        String nombre = sc.nextLine();
+        String nombre = sc.nextLine(); // Guardar el nombre ingresado por el usuario
 
-        JOptionPane.showMessageDialog(null, "Hola mundo.");
+        JOptionPane.showMessageDialog(null, "Hola mundo."); // Mostrar mensaje de bienvenida por la interfaz gráfica de JOptionPane
 
         
-        JOptionPane.showMessageDialog(null, "Bienvenido:" + nombre);
+        JOptionPane.showMessageDialog(null, "Bienvenido:" + nombre); //Mensaje personalizado con el nombre del usuario
 
 
 
